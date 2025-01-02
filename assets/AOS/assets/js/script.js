991145dbfd99068e2b89a54fe4499d8097bac824
certifieddev4real <certifieddeveloper6@gmail.com>
2025-01-02 06:09:33 +0100

    Earth 001
    11th(b) interstellar launch: just added the AOS test files, didn't want to create another repo just for that

---
+++ assets/AOS/assets/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Initialize AOS
+    AOS.init({
+      duration: 1000, // Animation duration (1 second)
+      once: false,    // Animations replay when scrolling back up
+    });
+  });  
